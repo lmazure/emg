df88b156f82549009e770e90027d1b10af76789c
Laurent <Laurent@gilgamesh>
2019-01-23 13:35:43 +0100

    Initial commit.

---
+++ src/main/java/fr/mazure/maven/emg/App.java
@@ -0,0 +1,18 @@
+package fr.mazure.maven.emg;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        HSSFWorkbook wb = new HSSFWorkbook();
+        System.out.println( "Hello World!" + wb);
+        
+    }
+}
---
+++ src/test/java/fr/mazure/maven/emg/Test.java
@@ -0,0 +1,12 @@
+package fr.mazure.maven.emg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+    @org.junit.jupiter.api.Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
